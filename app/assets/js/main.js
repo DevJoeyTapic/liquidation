5635d994a6eefe57ccdb379f8b73c5e572856991
jessadj <jessadj@wallem.com.ph>
2025-03-12 10:10:40 +0800

    config url

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -410,7 +410,7 @@ $(document).ready(function () {
         timestamp: timestamp
       },
       success: function(response) {
-        let baseUrl = 'http://192.168.192.251:3000';  // Fixed missing protocol
+        let baseUrl = 'https://agents.wallem.com.ph';  // Fixed missing protocol
         console.log(response);
         
         // Create the new note HTML
