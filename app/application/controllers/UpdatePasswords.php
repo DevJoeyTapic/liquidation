734c0ffee169434a25f6eb8977eaf219a76bca4d
jessadj <jessadj@wallem.com.ph>
2025-03-13 09:59:23 +0800

    access rights

--- app/application/controllers/UpdatePasswords.php
+++ app/application/controllers/UpdatePasswords.php
@@ -9,12 +9,13 @@ class UpdatePasswords extends CI_Controller {
     }
 
     public function index() {
-        $users = $this->User_model->get_users();
+        if ($this->session->userdata('user_type') == 1) {
+            $users = $this->User_model->get_users();
 
-        if ($users) {
+            if ($users) {
             foreach ($users as $user) {
                 if (password_verify($user->password, $user->password)) {
-                    continue;
+                continue;
                 }
 
                 $hashed_password = password_hash($user->password, PASSWORD_DEFAULT);
@@ -23,9 +24,13 @@ class UpdatePasswords extends CI_Controller {
 
             $this->session->set_flashdata('success', 'All user passwords have been updated to hashed passwords.');
             redirect('admin');
-        } else {
+            } else {
             $this->session->set_flashdata('error', 'No users found to update.');
             redirect('admin');
+            }
+        } else {
+            $this->session->set_flashdata('error', 'You do not have permission to perform this action.');
+            redirect('login');
         }
     }
 }
